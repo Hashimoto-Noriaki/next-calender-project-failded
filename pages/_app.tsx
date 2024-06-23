3cb71e90a13d1eab00a6b93baf0f27b0973017de
nori.hasimo.89@gmail.com <nori.hasimo.89@gmail.com>
2024-06-23 12:43:44 +0900

    Topページの作成1

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 // pages/_app.tsx
 import '../styles/destyle.css';
 import '../styles/index.css';
+import '../styles/output.css';
 import type { AppProps } from 'next/app';
 
 function MyApp({ Component, pageProps }: AppProps) {
